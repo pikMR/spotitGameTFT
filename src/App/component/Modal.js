e2482b3a66b2263516f5cb61649dd571a66cd043
pikmr <benjahergar@gmail.com>
2019-09-10 00:46:40 +0200

    css listado resultado y preparación de loop

--- src/App/component/Modal.js
+++ src/App/component/Modal.js
@@ -6,6 +6,16 @@ let id = new Date();
 const modal = (props) => {
     //const vacio = props.items.every((el) =>(el === undefined || el.length == 0));
     //const vacio = ((props.items === undefined || props.items.length == 0));
+    //const clases = Array.from(new Set(props.selecteds)).map(function(){ return this.clase });
+    //console.dir(clases);
+    var _elementos = props.selecteds && Array.from(new Set(props.selecteds));
+    var _clases = props.selecteds && Array.from(new Set(_elementos.map(function(elemento){ return elemento.clase })));
+    var _puntos = props.selecteds && Array.from(new Set(_elementos.map(function(elemento){ return elemento.puntos })));
+        console.log("-> clases: ");
+        console.dir(_clases);
+        console.log("-> puntos: ");
+        console.dir(_puntos);
+
     return (
         <div>
             <div className="modal-wrapper">
@@ -16,18 +26,40 @@ const modal = (props) => {
                     <p>
                         {props.children}
                     </p>
-                        <ul>
-                                  { props.selecteds && Array.from(new Set(props.selecteds)).map(
-                                  (elemento,index) => (
+
+                    <ul>
+                    {
+                        props.selecteds &&
+                            _puntos.map(function(comparapunto){ return
+                                _elementos.filter(x=>x.puntos === comparapunto).map((elemento,nindex)=>(
+                                        
+                                            <li key={id++}>"---- " {elemento.id}
+                                            </li>
+                                        )
+                                )
+                            })
+                    }
+                    </ul>
+
+                    <ul>
+                    {
+                    props.selecteds && Array.from(new Set(props.selecteds)).map(function(elemento,index){ return <div>
+                                            <li key={id++}>{elemento.clase}
+                                            </li>
+                                        </div> })
+                    }
+                    </ul>
+                    <ul>
+                              { 
+                                props.selecteds && Array.from(new Set(props.selecteds)).map(
+                              (elemento,index) => (
+                                    <div>
                                         <li key={id++}><img src={elemento.imagen} />
-                                        <div className="contentFlex">
-                                            <div className={'contentFlexNum' + ((elemento.puntos === 2) ? ' contentFlexNum_orange' : (elemento.puntos > 2) ? ' contentFlexNum_red':'')}>
-                                                {elemento.puntos}
-                                            </div>
-                                        </div>
-                                        </li>)
-                                    )}
-                        </ul>
+                                        </li>
+                                    </div>
+                                )
+                                )}
+                    </ul>
                 </div>
                 <div className="modal-footer">
                     <button className="btn-cancel" onClick={props.close}>COMPARTIR</button>
